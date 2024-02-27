b5a147d596e45129ae00770987cc3d92cacb012b
CadenasG <gonzalocadenas1@gmail.com>
2024-02-27 17:34:21 -0500

    (feat) Shared module and options

---
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+  items: MenuItem[] | undefined;
+  ngOnInit() {
+        this.items = [
+          {
+            label: 'Agendar Cita',
+            icon: 'pi pi-fw pi-calendar'
+          },
+          {
+            label: 'Usuarios',
+            icon: 'pi pi-fw pi-user',
+            items: [
+              {
+                label: 'Nuevo Usuario Psicologo',
+                icon: 'pi pi-fw pi-user-plus'
+              },
+              {
+                label: 'Nuevo Usuario Paciente',
+                icon: 'pi pi-fw pi-user-plus'
+              }
+            ]
+          },
+          {
+            label: 'Atender Cita',
+            icon: 'pi pi-fw pi-check-square'
+          }
+        ];
+      }
+}
