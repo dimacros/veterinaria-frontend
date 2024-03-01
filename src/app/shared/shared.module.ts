ce77a07aeb0cf9a87358ca6a6a2529566071fe23
Marcos David Huaranga Ortega <dimacros@hotmail.com>
2024-02-29 20:13:05 -0500

    update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MenubarModule } from 'primeng/menubar';
-import { CardModule } from 'primeng/card';
-import { TableModule } from 'primeng/table';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DropdownModule } from 'primeng/dropdown';
+import { EditorModule } from 'primeng/editor';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputNumberModule } from 'primeng/inputnumber';
+import { MenubarModule } from 'primeng/menubar';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { PasswordModule } from 'primeng/password';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { CardModule } from 'primeng/card';
+import { ChipModule } from 'primeng/chip';
+import { TableModule } from 'primeng/table';
+import { SkeletonModule } from 'primeng/skeleton';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-import { DropdownModule } from 'primeng/dropdown';
+import { StepsModule } from 'primeng/steps';
 
 
 @NgModule({
@@ -20,14 +33,29 @@ import { DropdownModule } from 'primeng/dropdown';
     MenubarModule
   ],
   exports: [
+    ButtonModule, 
+    AutoCompleteModule,
+    CalendarModule,
+    CheckboxModule,
+    DropdownModule,
+    EditorModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    MenubarModule,
+    MultiSelectModule,
+    PasswordModule,
+    RadioButtonModule,
+    ChipModule,
+    SkeletonModule,
     NavBarComponent,
     CardModule,
+    DropdownModule,
     TableModule,
-    InputTextModule,
-    InputNumberModule,
     ReactiveFormsModule,
-    ButtonModule,
-    DropdownModule
+    StepsModule,
   ]
 })
 export class SharedModule { }
